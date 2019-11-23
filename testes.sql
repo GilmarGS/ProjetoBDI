aff58f01509733adaaa8c044151be8ebf573e58d
deborafb <34282455+deborafb@users.noreply.github.com>
2019-11-23 18:36:19 -0300

    novos testes

--- testes.sql
+++ testes.sql
@@ -1,10 +1,17 @@
---Povoação das tabelas após correção
+INSERT INTO funcionario VALUES(19444, '11111111117', '7456321', 'jorge', 'RUA TAL', 210.3, 'RH', NULL, NULL);
+INSERT INTO funcionario VALUES(19454, '11111111117', '7456321', 'kedson', 'RUA TAL', 210.3, 'RH', 19444, NULL);
+
+INSERT INTO funcionario VALUES(19999, '11111111117', '7456321', 'jose', 'RUA TAL', 220.3, 'RH', NULL, NULL);
+INSERT INTO funcionario VALUES(18888, '11111145757', '7456321', 'raquel', 'RUA TAL', 199.3, 'RH', 19999, NULL);
 
 INSERT INTO funcionario VALUES(1144444, '22222222222', '7777777', 'PAULO', 'RUA TAL', 211.3, 'RH', NULL, NULL);
 INSERT INTO funcionario VALUES(115555, '11111111111', '7456321', 'ANA', 'RUA TAL', 111.3, 'RH', 1144444, NULL);
-INSERT INTO funcionario VALUES(115544, '11111111112', '7456321', 'joao', 'RUA TAL', 111.33, 'gerente', NULL, NULL);
-INSERT INTO funcionario VALUES(115577, '11111111113', '7456321', 'neco', 'RUA TAL', 11.33, 'padeiro', 115555, NULL);
+INSERT INTO funcionario VALUES(115544, '11111111112', '7456321', 'joao', 'RUA TAL', 111.33, 'RH', 1144444, NULL);
+INSERT INTO funcionario VALUES(115577, '11111111113', '7456321', 'neco', 'RUA TAL', 11.33, 'RH', 1144444, NULL);
 INSERT INTO funcionario VALUES(115533, '11111111114', '7456321', 'pedro', 'RUA TAL', 11.3, 'RH', 1144444, NULL);
+
+INSERT INTO funcionario VALUES(1155377, '11111111115', '7456321', 'pedro pereira', 'RUA TAL', 11.3, 'padeiro', 1144444, NULL);
+
 
 
 INSERT INTO filial VALUES(001,'Campina', 'RUA TAL tal', '333', 1144444);
@@ -22,6 +29,11 @@ INSERT INTO cliente VALUES('15055588855', 'carlos', 'carlos@gmail', 200, 'RUA TA
 
 
 INSERT INTO dependente VALUES('22222222222', '03/03/2004', 'PAULINHO',1144444);
+INSERT INTO dependente VALUES('22222222224', '03/03/2009', 'Carla',1144444);
+INSERT INTO dependente VALUES('22222222225', '03/03/2005', 'Anderson',1144444);
+INSERT INTO dependente VALUES('22222222234', '03/03/2018', 'Renata',115555);
+INSERT INTO dependente VALUES('22222222235', '03/03/1998', 'Ingrid',115555);
+
 
 INSERT INTO realiza_reclamacao VALUES(01, CURRENT_TIMESTAMP, 'muito ruim', 001, '11055588855');
 INSERT INTO realiza_reclamacao VALUES(02, CURRENT_TIMESTAMP, 'pessimo', 001,'11055588855');
@@ -30,17 +42,31 @@ INSERT INTO realiza_reclamacao VALUES(04,  CURRENT_TIMESTAMP, 'horrivel',002,'12
 INSERT INTO realiza_reclamacao VALUES(05, CURRENT_TIMESTAMP, 'vote', 002, '13055588855');
 
 
+
 INSERT INTO telefone_funcionario VALUES('0000-0000', 115555);
 
 INSERT INTO marca VALUES(3232, 'Rapoza');
+INSERT INTO marca VALUES(3233, 'SempreVerde');
+
 
 INSERT INTO categoria VALUES(44, 'Roupa');
+INSERT INTO categoria VALUES(46, 'Jardim');
 
 INSERT INTO caixa VALUES(0001, 001);
+INSERT INTO caixa VALUES(0002, 001);
+
 
 INSERT INTO equipamento VALUES(1111, 'equipamento da primeira filial', 0001);
+INSERT INTO equipamento VALUES(1112, 'equipamento da primeira filial', 0001);
+
 
 INSERT INTO realiza_manutencao VALUES(0002, 1111, 0001,  115555, CURRENT_TIMESTAMP,  1.22);
+INSERT INTO realiza_manutencao VALUES(0003, 1111, 0001,  115555, CURRENT_TIMESTAMP,  1.22);
+INSERT INTO realiza_manutencao VALUES(0004, 1112, 0001,  115555, CURRENT_TIMESTAMP,  1.22);
+INSERT INTO realiza_manutencao VALUES(0005, 1111, 0001,  1155377, CURRENT_TIMESTAMP,  1.22);
+
+
+
 
 INSERT INTO telefone_cliente VALUES('1111-11111', '11055588855');
 
@@ -55,6 +81,13 @@ INSERT INTO solicitacao VALUES(4, '03/03/2004', '03/03/2004', '03/03/2004', 5.00
 INSERT INTO nota_fiscal VALUES(02, '11-222-000', 10, '03/03/2004', 500.0, 4); 
 
 INSERT INTO produto VALUES(01, 'cristal', 'agua mineral', 0.50, 001, 2, 155.1, 200.0, '03/03/2011', '05/06/12', 3232, 44);
+INSERT INTO produto VALUES(02, 'reizinho', 'pipoca', 0.50, 001, 2, 80.1, 100.0, '03/03/2011', '05/06/12', 3232, 44);
+INSERT INTO produto VALUES(03, 'rexona', 'desodorante', 0.50, 001, 2, 40.1, 55.0, '03/03/2011', '05/06/12', 3232, 44);
+INSERT INTO produto VALUES(04, 'aaaa', 'peixe', 0.50, 001, 2, 45.1, 55.0, '03/03/2011', '05/06/12', 3233, 46);
+INSERT INTO produto VALUES(05, 'bbbb', 'peixe', 0.50, 001, 2, 40.1, 55.0, '03/03/2011', '05/06/12', 3233, 46);
+INSERT INTO produto VALUES(06, 'bbbb', 'peixe', 0.60, 001, 2, 40.1, 55.0, '03/03/2011', '05/06/12', 3233, 46);
+
 
 INSERT INTO item VALUES(111, 2, 02, 100, 0.10, 0.2);
 INSERT INTO item VALUES(112, 2, 02, 100, 0.10, 0.07);
+INSERT INTO item VALUES(113, 2, 02, 200, 0.10, 0.07);
